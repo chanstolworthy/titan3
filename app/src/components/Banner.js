d8a70760dccc1e59a829c34ca0030eaa0aa59f99
Chandler Stolworthy <chandler@eddyhr.com>
2017-11-21 09:11:10 -0700

    started banner

--- app/src/components/Banner.js
+++ app/src/components/Banner.js
@@ -4,8 +4,19 @@ import React from 'react';
 export default function Banner() {
 
     return(
-        <div>
-            Banner
+        <div className='banner_main_container'>
+            <div className=' container_width_standard'>
+
+            </div>
+            <div className='container_width_standard'>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div className='white_text'>Best Lorem Ipsum Tag Ever</div>
+                    <div></div>
+                </div>
+            </div>
         </div>
     )
 }
